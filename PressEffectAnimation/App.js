aa832925f0fe2e3897a79c1d4b80d0ec885f9d04
Handrew <andrew.a.han@gmail.com>
2018-08-14 19:38:06 -0400

    create components and move the presseffectAnimation there

--- PressEffectAnimation/App.js
+++ PressEffectAnimation/App.js
@@ -1,44 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableWithoutFeedback, Animated } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import PressEffectAnimation from './components/PressEffectAnimation';
 
 export default class App extends React.Component {
-  state = {
-    animatePress: new Animated.Value(1)
-  }
-  //method that shrinks the images
-  animateIn() {
-    Animated.timing(this.state.animatePress, {
-      toValue: 0.8, //the size value that it will shrink to.  In the state, there's the new Animated.Value with parameters of 1 which means the size of the image at it's normal size at which is 100%.
-      duration: 500
-    }).start()
-  }
-
-  animateOut() {
-    Animated.timing(this.state.animatePress, {
-      toValue: 1,
-      duration: 500
-    }).start()
-  }
-
+  
   render() {
     return (
       <View style={styles.container}>
 
-        <TouchableWithoutFeedback
-          onPressIn={() => this.animateIn()}
-          onPressOut={() => this.animateOut()}
-        >
-          <Animated.View style={{
-            width: 500, height: 500, backgroundColor: 'tomato',
-            transform: [
-              {
-                scale: this.state.animatePress
-              }
-            ]
-          }} >
-            <Image source={require('./assets/images/111.jpg')}></Image>
-          </Animated.View>
-        </TouchableWithoutFeedback>
+        <PressEffectAnimation imageofMe={require('./assets/images/111.jpg')} />
 
       </View>
     );
---
+++ PressEffectAnimation/components/PressEffectAnimation.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableWithoutFeedback, Animated } from 'react-native';
+
+export default class App extends React.Component {
+  state = {
+    animatePress: new Animated.Value(1)//Value 1 equates to 100% of the size from the start.
+  }
+  //method that shrinks the images
+  animateIn() {
+    Animated.timing(this.state.animatePress, {
+      toValue: 0.95, //the size value that it will shrink to.  In the state, there's the new Animated.Value with parameters of 1 which means the size of the image at it's normal size at which is 100%.
+      duration: 100
+    }).start()
+  }
+
+  animateOut() {
+    Animated.timing(this.state.animatePress, {
+      toValue: 1,
+      duration: 100
+    }).start()
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <TouchableWithoutFeedback
+          onPressIn={() => this.animateIn()}
+          onPressOut={() => this.animateOut()}
+        >
+          <Animated.View style={{
+            width: 500, height: 500, backgroundColor: 'tomato',
+            transform: [
+              {
+                scale: this.state.animatePress
+              }
+            ]
+          }} >
+            <Image style={{height: 500, width: 500}} source={this.props.imageofMe} />
+          </Animated.View>
+        </TouchableWithoutFeedback>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
