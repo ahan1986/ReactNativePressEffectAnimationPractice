ac8db2aee5f949fa609c173918adefbb4e27847c
Handrew <andrew.a.han@gmail.com>
2018-08-17 18:04:59 -0400

    create button that counts

--- PressEffectAnimation/components/OverLapBadge.js
+++ PressEffectAnimation/components/OverLapBadge.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Button, Animated } from 'react-native';
 
 export default class OverLapBadge extends React.Component {
+    state = {
+        badgeScale: new Animated.Value(0),
+        addedValue: 0
+    }
+
+    animatedBadge () {
+        Animated.timing(this.state.badgeScale, {
+            toValue: 1,
+            duration: 500
+        }).start();
+
+        const addingNewValue = this.state.addedValue + 1;
+        this.setState({
+            addedValue: addingNewValue
+        })
+        
+    }
 
     render() {
         return (
             <View style={styles.container}>
-                <View style={{ height: 100, width: 100, borderRadius: 50, backgroundColor: 'black' }}>
-                    <Text style={{ color: 'white', paddingLeft: 30, paddingTop: 40 }}>Hello</Text>
-                </View>
+                <Animated.View style={{ position: 'absolute', height: 100, width: 100, borderRadius: 50, backgroundColor: 'black', top: 50, left: 100, justifyContent: 'center', alignItems: 'center', borderColor: 'green', borderWidth: 10,
+                transform: [
+                    {
+                        scale: this.state.badgeScale
+                    }
+                ] }}>
+                    <Text style={{ color: 'white' }}>
+                        {this.state.addedValue}
+                    </Text>
+                </Animated.View>
+                <Button title='add' onPress={() => this.animatedBadge()}></Button>
             </View>
         );
     }
@@ -18,8 +43,8 @@ export default class OverLapBadge extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'red'
+        backgroundColor: 'red',
+        height: 200,
+        width: 200
     }
 })
